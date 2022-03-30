beeeb2e56a98861ce56ee609561782695109bd32
starkov_yp <starkov_yp@directum.ru>
2022-03-31 00:26:08 +0400

    Возможность выбора типа согласования при согласовании по регламенту [Rx]

--- GD.MainSolution/GD.MainSolution.Server/Condition/ConditionServerFunctions.cs
+++ GD.MainSolution/GD.MainSolution.Server/Condition/ConditionServerFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,14 @@ namespace GD.MainSolution.Server
 {
   partial class ConditionFunctions
   {
-
+public override string GetConditionName()
+    {
+      using (TenantInfo.Culture.SwitchTo())
+      {
+        if (_obj.ConditionType == ConditionType.IsParallel)
+          return GD.MainSolution.Conditions.Resources.IsParallel;        
+      }
+      return base.GetConditionName();
+    }
   }
 }
