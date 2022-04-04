845a646f9b0dd933fc77fcb15b6d6a93d3362983
starkov_yp <starkov_yp@directum.ru>
2022-04-04 20:05:36 +0400

    ПРЧ. Документ-основание, его рег. данные и исполнитель  в реестре [Rx]

--- GD.MainSolution/GD.MainSolution.Shared/ActionItemExecutionTask/ActionItemExecutionTaskHandlers.cs
+++ GD.MainSolution/GD.MainSolution.Shared/ActionItemExecutionTask/ActionItemExecutionTaskHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,23 @@ namespace GD.MainSolution
 {
   partial class ActionItemExecutionTaskSharedHandlers
   {
+
+    public override void DocumentsGroupAdded(Sungero.Workflow.Interfaces.AttachmentAddedEventArgs e)
+    {
+      base.DocumentsGroupAdded(e);
+      var document = Sungero.Docflow.OfficialDocuments.As(e.Attachment);
+      _obj.DocumentGD = document;
+      _obj.RegNumberGD = document.RegistrationNumber;
+      _obj.RegDateGD = document.RegistrationDate;
+    }
+
+    public override void DocumentsGroupDeleted(Sungero.Workflow.Interfaces.AttachmentDeletedEventArgs e)
+    {
+      base.DocumentsGroupDeleted(e);
+      _obj.DocumentGD = null;
+      _obj.RegNumberGD = null;
+      _obj.RegDateGD = null;
+    }
 
   }
 }
