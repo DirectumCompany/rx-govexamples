a80866b06430ac2235254c5c7b84c6643e618fb7
starkov_yp <starkov_yp@directum.ru>
2022-04-04 16:30:11 +0400

    Перекрытие Вх. писем, Вх. документов(base), Задачи на исполнение поручения, Задания на исполнение поручения,  [Rx]

---
+++ GD.MainSolution/GD.MainSolution.ClientBase/IncomingLetter/IncomingLetterClientFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using GD.MainSolution.IncomingLetter;
+
+namespace GD.MainSolution.Client
+{
+  partial class IncomingLetterFunctions
+  {
+
+  }
+}
