d77ae8fde9a7dfeca23fe3e58082fbf2ddb30454
starkov_yp <starkov_yp@directum.ru>
2022-04-05 17:11:06 +0400

    ПРЧ. НОР в составных поручениях [Rx]

--- GD.MainSolution/GD.MainSolution.ClientBase/ActionItemExecutionTask/ActionItemExecutionTaskHandlers.cs
+++ GD.MainSolution/GD.MainSolution.ClientBase/ActionItemExecutionTask/ActionItemExecutionTaskHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -7,6 +7,26 @@ using GD.MainSolution.ActionItemExecutionTask;
 
 namespace GD.MainSolution
 {
+  partial class ActionItemExecutionTaskActionItemPartsClientHandlers
+  {
+
+    public override void ActionItemPartsAssigneeValueInput(Sungero.RecordManagement.Client.ActionItemExecutionTaskActionItemPartsAssigneeValueInputEventArgs e)
+    {
+      base.ActionItemPartsAssigneeValueInput(e);
+      if (e.NewValue == null)
+        _obj.BusinessUnitGD = null;
+      
+      if (e.NewValue != null && e.NewValue.Department != null)
+        _obj.BusinessUnitGD = e.NewValue.Department.BusinessUnit;
+    }
+
+    public virtual void ActionItemPartsBusinessUnitGDValueInput(GD.MainSolution.Client.ActionItemExecutionTaskActionItemPartsBusinessUnitGDValueInputEventArgs e)
+    {
+      if (e.NewValue != null && _obj.Assignee == null)
+        _obj.Assignee = e.NewValue.CEO;
+    }
+  }
+
   partial class ActionItemExecutionTaskClientHandlers
   {
 
