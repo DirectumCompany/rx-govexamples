7d2e82af5fbefaa40e61a2cce4b1440564e28bf0
Подлевских Татьяна <podlevskikh_ta@directum.ru>
2022-07-07 15:50:16 +0400

    Срок исполнения во входящих письмах [Rx]

--- GD.MainSolution/GD.MainSolution.Shared/IncomingLetter/IncomingLetterSharedFunctions.cs
+++ GD.MainSolution/GD.MainSolution.Shared/IncomingLetter/IncomingLetterSharedFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,22 @@ namespace GD.MainSolution.Shared
 {
   partial class IncomingLetterFunctions
   {
+    public override void DocumentAttachedInMainGroup(Sungero.Workflow.ITask task)
+    {
+      base.DocumentAttachedInMainGroup(task);
+      
+      if (Sungero.RecordManagement.ActionItemExecutionTasks.Is(task))
+      {
+        var actionItem = Sungero.RecordManagement.ActionItemExecutionTasks.As(task);
+        if (_obj.DeadlineGD != null)
+        {
+          if (actionItem.IsCompoundActionItem != true && actionItem.Deadline == null)
+            actionItem.Deadline = _obj.DeadlineGD;
+          else if (actionItem.IsCompoundActionItem == true && actionItem.FinalDeadline == null)
+            actionItem.FinalDeadline = _obj.DeadlineGD;
+        }
+      }
+    }
 
   }
 }
