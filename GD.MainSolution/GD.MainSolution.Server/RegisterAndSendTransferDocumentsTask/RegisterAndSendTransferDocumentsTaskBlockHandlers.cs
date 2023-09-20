b7130509abf0c0cbf4e9c657fd1658f3c92e0d1f
Pushina TL <Pushina_TL@directum.ru>
2023-09-20 09:01:17 +0400

    Перекрытие задачи RegisterAndSendTransferDocument

---
+++ GD.MainSolution/GD.MainSolution.Server/RegisterAndSendTransferDocumentsTask/RegisterAndSendTransferDocumentsTaskBlockHandlers.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Workflow;
+using GD.MainSolution.RegisterAndSendTransferDocumentsTask;
+
+namespace GD.MainSolution.Server.RegisterAndSendTransferDocumentsTaskBlocks
+{
+  partial class SearchForCoordinatesHandlers
+  {
+
+  }
+  partial class RegisterAndSendHandlers
+  {
+
+  }
+}
---
+++ GD.MainSolution/GD.MainSolution.Server/RegisterAndSendTransferDocumentsTask/RegisterAndSendTransferDocumentsTaskRouteHandlers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Workflow;
+using GD.MainSolution.RegisterAndSendTransferDocumentsTask;
+
+namespace GD.MainSolution.Server
+{
+  partial class RegisterAndSendTransferDocumentsTaskRouteHandlers
+  {
+
+  }
+}
