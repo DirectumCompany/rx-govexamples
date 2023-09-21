c0d9e09b86228b99155396ce9269a50466fc9c90
Pushina TL <Pushina_TL@directum.ru>
2023-09-21 12:01:50 +0400

    После рецензирования

--- GD.MainSolution/GD.MainSolution.Server/RegisterAndSendTransferDocumentsTask/RegisterAndSendTransferDocumentsTaskBlockHandlers.cs
+++ GD.MainSolution/GD.MainSolution.Server/RegisterAndSendTransferDocumentsTask/RegisterAndSendTransferDocumentsTaskBlockHandlers.cs
@@ -13,30 +13,17 @@ namespace GD.MainSolution.Server.RegisterAndSendTransferDocumentsTaskBlocks
 
     public override void RegisterAndSendEnd(System.Collections.Generic.IEnumerable<GD.CitizenRequests.IRegisterAndSendTransferDocumentsAssignment> createdAssignments)
     {
-      // Обработка перенаправления.
-      var assignment = createdAssignments.OrderByDescending(a => a.Created).FirstOrDefault();
-      // Если результат выполнения равен "Выполнен".
-      if (assignment.Result == CitizenRequests.RegisterAndSendTransferDocumentsAssignment.Result.Complete)
+      base.RegisterAndSendEnd(createdAssignments);
+      
+      var taskItem = MainSolution.ActionItemExecutionTasks.As(_obj.MainTask);
+      if (taskItem != null)
       {
-        var taskReview = GovernmentSolution.DocumentReviewTasks.As(_obj.MainTask);
-        if (taskReview != null)
-        {
-          var actionItemTask = taskReview.ResolutionGroup.ActionItemExecutionTasks.Any() ?
-            GovernmentSolution.ActionItemExecutionTasks.As(taskReview.ResolutionGroup.ActionItemExecutionTasks.FirstOrDefault()) :
-            CitizenRequests.PublicFunctions.Module.Remote.GetActualActionItemExecutionTaskByReview(taskReview);
-          if (actionItemTask != null)
-            GovernmentSolution.PublicFunctions.ActionItemExecutionTask.SetDocumentStates(actionItemTask);
-        }
-        else
-        {
-          var taskItem = MainSolution.ActionItemExecutionTasks.As(_obj.MainTask);
-          var actionItemTask = taskItem.DraftActionItemGD != null ? GovernmentSolution.ActionItemExecutionTasks.As(taskItem.DraftActionItemGD) :
-            GD.MainSolution.Module.CitizenRequests.PublicFunctions.Module.Remote.GetActualActionItemExecutionTask(taskItem);          
-          if (actionItemTask != null)
-            GovernmentSolution.PublicFunctions.ActionItemExecutionTask.SetDocumentStates(actionItemTask);
-        }
+        var actionItemTask = taskItem.DraftActionItemGD != null ? GovernmentSolution.ActionItemExecutionTasks.As(taskItem.DraftActionItemGD) :
+          GD.MainSolution.Module.CitizenRequests.PublicFunctions.Module.Remote.GetActualActionItemExecutionTask(taskItem);
+        if (actionItemTask != null)
+          GovernmentSolution.PublicFunctions.ActionItemExecutionTask.SetDocumentStates(actionItemTask);
       }
-    }
 
+    }
   }
 }
