c235f1a4f779f4e40cc3f8791e9a265768caa703
savin_aa <savin_aa@directum.ru>
2024-08-22 10:19:18 +0400

    Новый справочник "Классификация поручений" и его инициализация в модуле.

--- GD.MainEDMS/GD.MainEDMS.Server/ModuleInitializer.cs
+++ GD.MainEDMS/GD.MainEDMS.Server/ModuleInitializer.cs
@@ -9,5 +9,47 @@ namespace GD.MainEDMS.Server
 {
   public partial class ModuleInitializer
   {
+
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      
+      var allUsers = Roles.AllUsers;
+      
+      // Справочники.
+      GrantRightsForAllUsers(allUsers);
+      
+      // Выдача прав роли "Делопроизводители".
+      GrantRightsToClerksRole();
+      
+    }
+    
+    /// <summary>
+    /// Выдать права всем пользователям.
+    /// </summary>
+    /// <param name="allUsers">Группа "Все пользователи".</param>
+    public static void GrantRightsForAllUsers(IRole allUsers)
+    {
+      InitializationLogger.Debug("Init: Grant rights on databooks to all users.");
+      
+      // Классфикация поручений
+      AssignmentClassifications.AccessRights.Grant(allUsers, DefaultAccessRightsTypes.Read);
+      AssignmentClassifications.AccessRights.Save();
+    }
+    
+    /// <summary>
+    /// Назначить права роли "Делопроизводители".
+    /// </summary>
+    public static void GrantRightsToClerksRole()
+    {
+      InitializationLogger.Debug("Init: Grant rights on documents and databooks to clerks");
+      
+      var clerks = Sungero.Docflow.PublicFunctions.DocumentRegister.Remote.GetClerks();
+      if (clerks == null)
+        return;
+      
+      // Классфикация поручений
+      AssignmentClassifications.AccessRights.Grant(clerks, DefaultAccessRightsTypes.Change);
+      AssignmentClassifications.AccessRights.Save();
+    }
   }
 }
