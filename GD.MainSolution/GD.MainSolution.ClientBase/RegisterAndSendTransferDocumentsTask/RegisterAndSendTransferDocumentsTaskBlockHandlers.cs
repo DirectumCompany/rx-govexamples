a17e05502a3092e0a096283f9140de5311f3bbfe
savin_aa <savin_aa@directum.ru>
2025-04-04 08:20:43 +0400

    Обновление на 4.12

---
+++ GD.MainSolution/GD.MainSolution.ClientBase/RegisterAndSendTransferDocumentsTask/RegisterAndSendTransferDocumentsTaskBlockHandlers.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using GD.MainSolution.RegisterAndSendTransferDocumentsTask;
+
+namespace GD.MainSolution.Client.RegisterAndSendTransferDocumentsTaskBlocks
+{
+  partial class SearchForCoordinatesHandlers
+  {
+
+  }
+
+  partial class RegisterAndSendHandlers
+  {
+
+  }
+}
